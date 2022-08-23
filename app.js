24f3d33151b598b10e9226c68c0d4b08ba6641cc
Mathieu Roberts <mathieulroberts@gmail.com>
2022-08-23 10:37:28 -0700

    added baseline

---
+++ app.js
@@ -0,0 +1,42 @@
+'use strict' ;
+
+let seattle = {
+    minCust: 23,
+    maxCust: 65,
+    avgSell: 6.3,
+    
+}
+
+let tokyo = {
+    minCust: 3,
+    maxCust: 24,
+    avgSell: 1.2,
+}
+
+let dubai = {
+    minCust: 11,
+    maxCust: 38,
+    avgSell: 3.7,
+}
+
+let paris = {
+    minCust: 20,
+    maxCust: 38,
+    avgSell: 2.3,
+}
+
+let lima = {
+    minCust: 2,
+    maxCust: 16,
+    avgSell: 4.6,
+}
+
+
+
+
+
+
+
+
+
+
