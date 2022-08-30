3e8e4750debebaa8266ea822675e5b249336d3c3
Mathieu Roberts <mathieulroberts@gmail.com>
2022-08-29 19:55:03 -0700

    Finally got the table to refresh

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // global variables
 let hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '7pm'];
 let tableHours = ['Locations','6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '7pm', 'Total'];
+let tableId = document.getElementById('table');
 
 // location object constructor function
 function Location (locName, minCust, maxCust, avgSell) {
@@ -31,59 +32,59 @@ let getId = document.getElementById('form');
 getId.addEventListener('submit', function (event) {
   event.preventDefault(); //prevents page refresh
   locArray.push(new Location(event.target.location_name.value, event.target.low_cust.value, event.target.high_cust.value, event.target.avg_cook.value))
-  
+  tableId.innerHTML = '';
+  makeTable();
 })
 
-let tableId = document.getElementById('table');
+function makeTable () {
+  // Table header
+  for (let i = 0; i < tableHours.length; i++ ) {
+    let tableHead = document.createElement('th');
+    tableId.appendChild(tableHead);
+    tableHead.textContent = tableHours[i];
+  }
 
-// Table header
-for (let i = 0; i < tableHours.length; i++ ) {
-  let tableHead = document.createElement('th');
-  tableId.appendChild(tableHead);
-  tableHead.textContent = tableHours[i];
-}
+  //table data
+  for (let i = 0; i < locArray.length; i++) {
+    let total = 0
+    let tableHead = document.createElement('th');
+    let tableRow = document.createElement('tr');
+    tableId.appendChild(tableRow);
+    tableId.appendChild(tableHead);
+    tableHead.textContent = locArray[i].locName
 
-//table data
-for (let i = 0; i < locArray.length; i++) {
-  let total = 0
-  let tableHead = document.createElement('th');
-  let tableRow = document.createElement('tr');
-  tableId.appendChild(tableRow);
-  tableId.appendChild(tableHead);
-  tableHead.textContent = locArray[i].locName
+    for (let j = 0; j < hoursOpen.length; j++) {
+      let storedNumb = locArray[i].randNumCust();
+      locArray[i].sellStor.push(storedNumb);
+      let tableData = document.createElement('td');
+      tableId.appendChild(tableData);
+      tableData.textContent = storedNumb;
+      total += storedNumb;
+    }
 
-  for (let j = 0; j < hoursOpen.length; j++) {
-    let storedNumb = locArray[i].randNumCust();
-    locArray[i].sellStor.push(storedNumb);
     let tableData = document.createElement('td');
     tableId.appendChild(tableData);
-    tableData.textContent = storedNumb;
-    total += storedNumb;
+    tableData.textContent = total;
+    locArray[i].sellStor.push(total);
   }
 
-  let tableData = document.createElement('td');
-  tableId.appendChild(tableData);
-  tableData.textContent = total;
-  locArray[i].sellStor.push(total);
-}
+  // grand totals
+  let tableRow = document.createElement('tr');
+  let tableHead = document.createElement('th');
+  tableId.appendChild(tableRow);
+  tableId.appendChild(tableHead);
+  tableHead.textContent = 'Totals';
 
-// grand totals
-let tableRow = document.createElement('tr');
-let tableHead = document.createElement('th');
-tableId.appendChild(tableRow);
-tableId.appendChild(tableHead);
-tableHead.textContent = 'Totals';
-
-for (let j = 0; j < 14; j++) {
-  let hourTot = 0;
-  for (let i = 0; i < locArray.length; i++) {
-    hourTot += locArray[i].sellStor[j];
+  for (let j = 0; j < 14; j++) {
+    let hourTot = 0;
+    for (let i = 0; i < locArray.length; i++) {
+      hourTot += locArray[i].sellStor[j];
+    }
+    let tableData = document.createElement('td');
+    tableId.appendChild(tableData);
+    tableData.textContent = hourTot;
   }
-  let tableData = document.createElement('td');
-  tableId.appendChild(tableData);
-  tableData.textContent = hourTot;
 }
 
-
-
+makeTable();
 
